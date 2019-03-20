17395fe2265d760add90fa759687f94bf57f1184
AndersonHqds <andyhhenrikefs@gmail.com>
2019-03-20 00:51:17 -0300

    Commit inicial

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="admin-pages">
+        <PageTitle icon="fa fa-cogs" main="System administration"
+            sub="Registers & Cia" />
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Articles">
+                        <ArticleAdmin />
+                    </b-tab>
+                    <b-tab title="Categories">
+                        <CategoryAdmin />
+                    </b-tab>
+                    <b-tab title="Users">
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
+       
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+import ArticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
+
+export default {
+    name: 'AdminPages',
+    components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
+}
+</script>
+
+<style>
+
+</style>
